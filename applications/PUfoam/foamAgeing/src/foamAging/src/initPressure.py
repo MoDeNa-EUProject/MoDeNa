793fd53dfc7e21910a6ca98e37605b331157b577
Pavel Ferkl <pavel.ferkl@vscht.cz>
2015-09-22 14:50:28 +0200

    adding foam ageing application

---
+++ applications/PUfoam/foamAgeing/src/foamAging/src/initPressure.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 28 22:21:41 2015
+
+@author: Pavel Ferkl
+"""
+
+p=101325
+xcd=4e-4
+xn=0.79
+xo=1-xcd-xn
+pcd=xcd*p
+pn=p*xn
+po=p*xo
+print 'carbon dioxide pressure: {0:.3f}'.format(pcd)
+print 'nitrogen pressure: {0:.3f}'.format(pn)
+print 'oxygen pressure: {0:.3f}'.format(po)
