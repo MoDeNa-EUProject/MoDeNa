6d097a2cdb5d8d7037830508ca58e3d2f276d50a
Mohammad Marvi-Mashhadi <mohamadmarvi@gmail.com>
2016-01-17 16:48:59 +0100

    Replacing SpherePack.exe with internal code developed by me

--- applications/PUfoam/Models/FoamElasticModulus/FoamElasticModulus.py
+++ applications/PUfoam/Models/FoamElasticModulus/FoamElasticModulus.py
@@ -1,4 +1,4 @@
-__author__ = 'imdea-Mohammad Marvi Mashhadi: mohammad.marvi@imdea.org'
+__author__ = 'IMDEA Materials-Mohammad Marvi Mashhadi: mohammad.marvi@imdea.org'
 import os
 import os.path
 from itertools import groupby
@@ -8,6 +8,7 @@ from scipy.integrate import quad
 import time
 import commands
 import glob
+import random
 # Main Inputs:
 MU=0.39252             #Mean value of cell size distribution
 SIGMA=0.1568           #Standard deviation of cell size distribution
@@ -18,7 +19,7 @@ NumOfCells=50
 MeshSize=0.1
 AppliedStrain=-0.01
 DensityOfPUFoam=30.20  #Real density of foam (kg/m3)
-PUinWall=0.60          #Amount of material in walls (in percentage %)
+PUinWall=0.70          #Amount of material in walls (in percentage %)
 ############################################################
 #Material Elasticity properties:
 G='0.9e9'
@@ -33,211 +34,222 @@ A4=(-0.2291)
 A5=1.0255
 ############################################################
 #####To create input file for SpherePack:
-mypath=os.getcwd()
-myfile=os.path.join(mypath,'Project01.prj')
-f=open(myfile,'w')
-f.write('{0:34s}\n'.format('# RANDOM COORDINATES: 0 OR FILE: 1'))
-f.write('{0}\n'.format('0'))
-f.write('{0}\n'.format('# NUMBER OF SPHERES'))
-f.write('{0:d}\n'.format(NumOfCells))
-f.write('{0}\n'.format('# LENGTH IN X-, Y- AND Z-DIRECTION'))
-f.write('{0:f}\n'.format(4.5))
-f.write('{0:f}\n'.format(4.5))
-f.write('{0:f}\n'.format(4.5))
-f.write('{0}\n'.format('# EPSILON'))
-f.write('{0}\n'.format('0.0001'))
-f.write('{0}\n'.format('# NTAU'))
-f.write('{0}\n'.format('963800000'))
-f.write('{0}\n'.format('# NOMINAL DENSITY'))
-f.write('{0}\n'.format('0.50'))
-f.write('{0}\n'.format('# MAX. AND MINIM. DIAMETER'))
-f.write('{0}\n'.format('3.0'))
-f.write('{0}\n'.format('1.0'))
-f.write('{0}\n'.format('# MAX. NUMBER OF STEPS'))
-f.write('{0}\n'.format('50000000'))
-f.write('{0}\n'.format('# DISTRIBUTION'))
-f.write('{0}\n'.format('3'))
-f.write('{0}\n'.format('# DISTRIBUTION PARAMETERS'))
-f.write('{0:f}\n'.format(MU))
-f.write('{0:f}\n'.format(SIGMA))
-f.write('{0}\n'.format('0.4'))
-f.write('{0}\n'.format('-3.3'))
-f.write('{0}\n'.format('3'))
-f.write('{0}\n'.format('0.10'))
-f.write('{0}\n'.format('0.20'))
-f.write('{0}\n'.format('0.70'))
-f.write('{0}\n'.format('0.902113'))
-f.write('{0}\n'.format('3.000000'))
-f.write('{0}\n'.format('1.000000'))
-f.close()
-###########################################################
-os.chdir(mypath)
-command1='wine SpherePackFB.exe'
-os.system(command1)
-while True:
-    time.sleep(1)
-    files = os.listdir('.')
-    if ('Project01.rco' in files):
-        break
-myfile12=os.path.join(mypath,'Project01.rco')
-CentersRads=np.loadtxt(myfile12,usecols = (0,1,2,3))
-Centers=CentersRads[:,:3]  #All centers of spheres
-Rads=CentersRads[:,3]      #All radii of spheres
-Rads=Rads/2
-MaxCenters=np.amax(Centers, axis=0)
-L2=int(0.5+MaxCenters[0])
-X=L2+Centers[:,0]
-Y=L2+Centers[:,1]
-Z=L2+Centers[:,2]
-Centers27=np.array([X,X+L2,X-L2,X+L2,X,X+L2,X,X-L2,X-L2,X-L2,X+L2,X,X,X-L2,X+L2,X+L2,X,X,X-L2,X,X,X-L2,X+L2,X+L2,X-L2,X-L2,X+L2,Y,Y+L2,Y-L2,Y+L2,Y+L2,Y,Y-L2,Y,Y-L2,Y+L2,Y-L2,Y-L2,Y+L2,Y,Y,Y,Y+L2,Y,Y,Y-L2,Y,Y+L2,Y-L2,Y+L2,Y-L2,Y+L2,Y-L2,Z,Z+L2,Z-L2,Z,Z+L2,Z+L2,Z-L2,Z-L2,Z,Z,Z,Z+L2,Z-L2,Z+L2,Z-L2,Z,Z,Z+L2,Z,Z,Z-L2,Z+L2,Z+L2,Z-L2,Z+L2,Z-L2,Z-L2])
-# Creation of input .txt files for neper
-myfile3=os.path.join(mypath,'Centers.txt')
-ff=open(myfile3,'w')
-for i in range(0,27):
-    for j in range(0,NumOfCells):
-        ff.write('{0:f}\t{1:f}\t{2:f}\n'.format(Centers27[i,j],Centers27[i+27,j],Centers27[i+54,j]))
-ff.close()
-myfile4=os.path.join(mypath,'Rads.txt')
-fff=open(myfile4,'w')
-for i in range(0,27):
-    for j in range(0,NumOfCells):
-        fff.write('{0:f}\n'.format(Rads[j]))
-fff.close()
-commandTessellation="neper -T -n {0:d} -domain 'cube(14,14,14)' -morpho @Centers.txt -weight @Rads.txt -regularization 1 -mloop 15 -o RVE27 -format geo -statcell vol -statedge length -statface area -statver x".format((27*NumOfCells))
-os.system(commandTessellation)
-while True:
-    time.sleep(1)
-    files = os.listdir('.')
-    if ('RVE27.geo' in files):
-        break
-################################################################
-######Extraction of middle Representative volume element########
-################################################################
-myfile12=os.path.join(mypath,'RVE27.stver')
-verX=np.loadtxt(myfile12)
-NumOfNodes=len(verX)
-myfile12=os.path.join(mypath,'RVE27.stedge')
-EdgesLength=np.loadtxt(myfile12)
-NumOfEdges=len(EdgesLength)
-myfile12=os.path.join(mypath,'RVE27.stface')
-SurfacesArea=np.loadtxt(myfile12)
-NumOfSurfaces=len(SurfacesArea)
-myfile12=os.path.join(mypath,'RVE27.stcell')
-CellVolumes=np.loadtxt(myfile12)
-NumOfVolumes=len(CellVolumes)
-####   Reading the GEO file
-myfile12=os.path.join(mypath,'RVE27.geo')
-text_file = open(myfile12, "r")
-lines = text_file.readlines()
-####   Extraction of Nodes
-t=0
-Nodes=list(range(NumOfNodes))
-for i in range(0,NumOfNodes):
-    currentline = lines[i].split("{")
-    a=currentline[1].split("}")
-    b=a[0].split(",")
-    c=np.array(b)
-    Nodes[t]=np.absolute(c.astype(np.float))
-    t=t+1
-#####   Extraction of Edges
-Edges=list(range(NumOfEdges))
-r=0
-t=0
-for i in range(NumOfNodes,NumOfNodes+NumOfEdges):
-    currentline = lines[i].split("{")
-    a=currentline[1].split("}")
-    b=a[0].split(",")
-    c=np.array(b)
-    Edges[t]=np.absolute(c.astype(np.float))
-    t=t+1
-####   Extraction of Faces
-Faces=list(range(NumOfSurfaces))
-r=0
-t=0
-for i in range(0,NumOfSurfaces):
-    j=NumOfNodes+NumOfEdges+(2*i)
-    currentline = lines[j].split("{")
-    a=currentline[1].split("}")
-    b=a[0].split(",")
-    c=np.array(b)
-    Faces[t]=np.absolute(c.astype(np.float))
-    t=t+1
-####   Extraction of volumes
-Volumes=list(range(NumOfVolumes))
-a=list(lines[NumOfNodes+NumOfEdges+(2*NumOfSurfaces)])
-a[0:21]=''
-o=len(a)
-a[o-3:]=''
-lines[NumOfNodes+NumOfEdges+(2*NumOfSurfaces)]="".join(a)
-currentline=lines[NumOfNodes+NumOfEdges+(2*NumOfSurfaces)].split(",")
-c=np.array(currentline)
-Volumes[0]=np.absolute(c.astype(np.float))
-t=1
-for i in range(1,NumOfVolumes):
-    j=NumOfNodes+NumOfEdges+(2*NumOfSurfaces)+i
-    currentline = lines[j].split(" = {")
-    a=currentline[2].split("}")
-    b=a[0].split(",")
-    c=np.array(b)
-    Volumes[t]=np.absolute(c.astype(np.float))
-    t=t+1
-#####################################################
-MAX0=list(range(0,NumOfCells))
-for i in range(NumOfCells):
-    MAX0[i]=max(Volumes[i])
-MaxIndexOfFaces=max(MAX0)
-MAX1=list(range(0,int(MaxIndexOfFaces)))
-for i in range(int(MaxIndexOfFaces)):
-    MAX1[i]=max(Faces[i])
-MaxIndexOfEdges=max(MAX1)
-MAX2=list(range(0,int(MaxIndexOfEdges)))
-for i in range(int(MaxIndexOfEdges)):
-    MAX2[i]=max(Edges[i])
-MaxIndexOfNodes=max(MAX2)
-text_file.close()
-####################################################
-# Making GEO file containing Periodic RVE
-myfile13=os.path.join(mypath,'PeriodicRVE.geo')
-text_GEO = open(myfile13, "w")
-text_GEO.write('{0}{1}{2}\n'.format('cl=',MeshSize,';'))
-for i in range(int(MaxIndexOfNodes)):
-    lines[i] = lines[i].replace('};',',cl};')
-    text_GEO.write('{0}'.format(lines[i]))
-for i in range(int(NumOfNodes),int(NumOfNodes+MaxIndexOfEdges)):
-    text_GEO.write('{0}'.format(lines[i]))
-j=int(NumOfNodes+NumOfEdges)
-for i in range(int(NumOfNodes+NumOfEdges),int(NumOfNodes+NumOfEdges+MaxIndexOfFaces)):
-    text_GEO.write('{0}{1}'.format(lines[j],lines[j+1]))
-    j=2+j
-text_GEO.write('{0}{1}{2}\n'.format(' Surface Loop (1) = {',lines[int(NumOfNodes+NumOfEdges+(2*NumOfSurfaces))],'};'))
-NumOfCells=NumOfVolumes/27
-j=int(NumOfNodes+NumOfEdges+(2*NumOfSurfaces)+1)
-for i in range(int(NumOfCells-1)):
-    text_GEO.write('{0}'.format(lines[j]))
-    j+=1
-text_GEO.write('{0}{1}{2}{3}{4}'.format('Volume (',NumOfCells,') = {',NumOfCells,'};'))
-text_GEO.close()
-################################ End of Geometry construction##################################
-os.remove('Project01.prj')
-os.remove('Project01.rco')
-os.remove('Project01.rst')
-os.remove('Project01.sco')
-os.remove('Rads.txt')
-os.remove('Centers.txt')
-os.remove('RVE27.geo')
-os.remove('RVE27.stcell')
-os.remove('RVE27.stedge')
-os.remove('RVE27.stface')
-os.remove('RVE27.stver')
-######################################################################
-                  # Creating Mesh Using GMSH
-######################################################################
-commandMeshing="gmsh PeriodicRVE.geo -2 -optimize_lloyd -o PeriodicRVE -format inp -saveall"
-os.system(commandMeshing)
-while True:
-    time.sleep(1)
+SuccessInMeshing=1
+while SuccessInMeshing==1:
+    SuccessInTessellation=1
+    while SuccessInTessellation==1:
+        NumSpheres=NumOfCells
+        Rad=abs((np.random.normal(MU, SIGMA,NumSpheres)))
+        Rads1=list(range(NumSpheres))
+        t=0
+        for i in range(NumSpheres):
+            c=abs(Rad[t])
+            Rads1[t]=c.astype(np.float)
+            t=t+1
+        Rads1=sorted(Rads1)
+        v=0.00
+        for i in range(NumSpheres):
+            v=v+((2.00*Rads1[i])**3.00)
+        centers = [[0 for i in range(3)] for j in range(NumSpheres)]
+        v=v*1.40
+        lc=v**(1.00/3.00)
+        K=0
+        while K==0:
+            j=-1
+            h=0
+            timeout = time.time() + 10
+            while NumSpheres>=j and h==0:
+                if time.time()>timeout:
+                    h=1
+                    break
+                j=j+1
+                if j==NumSpheres:
+                    K=1
+                    break
+                PickCenterX,PickCenterY,PickCenterZ=lc*random.random(),lc*random.random(),lc*random.random()
+                while (lc-Rads1[j]>=PickCenterX and lc-Rads1[j]>=PickCenterY and lc-Rads1[j]>=PickCenterZ and Rads1[j]<PickCenterX and Rads1[j]<PickCenterY and Rads1[j]<PickCenterZ):
+                    PickCenterX,PickCenterY,PickCenterZ=lc*random.random(),lc*random.random(),lc*random.random()
+                centers[j][0],centers[j][1],centers[j][2]=PickCenterX,PickCenterY,PickCenterZ
+                KeepCentreX, KeepCentreY, KeepCentreZ, KeepR=PickCenterX, PickCenterY, PickCenterZ, Rads1[j]
+                if j>0:
+                    for t in range(0,j):
+                        if (((( ((KeepCentreX-centers[t][0])**2.00)+((KeepCentreY-centers[t][1])**2.00)+((KeepCentreZ-centers[t][2])**2.00))**0.50)-(KeepR+Rads1[t]))<0.000) and t!=j:
+                            centers[j][0],centers[j][0],centers[j][0]=0,0,0
+                            j=j-1
+                            break
+        mypath=os.getcwd()
+        myfile=os.path.join(mypath,'Project01.rco')
+        f=open(myfile,'w')
+        for i in range(NumSpheres):
+            f.write('{0:f}{1}{2:f}{3}{4:f}{5}{6:f}\n'.format(centers[i][0],'	',centers[i][1],'	',centers[i][2],'	',2.0*Rads1[i]))
+        f.close()
+        MAXcenters=max(centers)
+        Mincenters=min(centers)
+        EdgeCubeSize=[math.ceil(MAXcenters[0]-Mincenters[0]),math.ceil(MAXcenters[1]-Mincenters[1]),math.ceil(MAXcenters[2]-Mincenters[2])]
+        EdgeRVESize=int(3.5*max(EdgeCubeSize)) #For NEPER: Size of edge of RVE
+        ###########################################################
+        os.chdir(mypath)
+        while True:
+            time.sleep(1)
+            files = os.listdir('.')
+            if ('Project01.rco' in files):
+                break
+        myfile12=os.path.join(mypath,'Project01.rco')
+        CentersRads=np.loadtxt(myfile12,usecols = (0,1,2,3))
+        os.remove('Project01.rco')
+        Centers=CentersRads[:,:3]  #All centers of spheres
+        Rads=CentersRads[:,3]      #All radii of spheres
+        Rads=Rads/2
+        MaxCenters=np.amax(Centers, axis=0)
+        L2=int(0.5+MaxCenters[0])
+        X=L2+Centers[:,0]
+        Y=L2+Centers[:,1]
+        Z=L2+Centers[:,2]
+        Centers27=np.array([X,X+L2,X-L2,X+L2,X,X+L2,X,X-L2,X-L2,X-L2,X+L2,X,X,X-L2,X+L2,X+L2,X,X,X-L2,X,X,X-L2,X+L2,X+L2,X-L2,X-L2,X+L2,Y,Y+L2,Y-L2,Y+L2,Y+L2,Y,Y-L2,Y,Y-L2,Y+L2,Y-L2,Y-L2,Y+L2,Y,Y,Y,Y+L2,Y,Y,Y-L2,Y,Y+L2,Y-L2,Y+L2,Y-L2,Y+L2,Y-L2,Z,Z+L2,Z-L2,Z,Z+L2,Z+L2,Z-L2,Z-L2,Z,Z,Z,Z+L2,Z-L2,Z+L2,Z-L2,Z,Z,Z+L2,Z,Z,Z-L2,Z+L2,Z+L2,Z-L2,Z+L2,Z-L2,Z-L2])
+        # Creation of input .txt files for neper
+        myfile3=os.path.join(mypath,'Centers.txt')
+        ff=open(myfile3,'w')
+        for i in range(0,27):
+            for j in range(0,NumOfCells):
+                ff.write('{0:f}\t{1:f}\t{2:f}\n'.format(Centers27[i,j],Centers27[i+27,j],Centers27[i+54,j]))
+        ff.close()
+        myfile4=os.path.join(mypath,'Rads.txt')
+        fff=open(myfile4,'w')
+        for i in range(0,27):
+            for j in range(0,NumOfCells):
+                fff.write('{0:f}\n'.format(Rads[j]))
+        fff.close()
+        commandTessellation="neper -T -n {0:d} -domain 'cube({1:d},{2:d},{3:d})' -morpho @Centers.txt -weight @Rads.txt -regularization 1 -mloop 15 -o RVE27 -format geo -statcell vol -statedge length -statface area -statver x".format((27*NumSpheres),EdgeRVESize,EdgeRVESize,EdgeRVESize)
+        os.system(commandTessellation)
+        time.sleep(5)
+        files = os.listdir('.')
+        if ('RVE27.geo' in files):
+            SuccessInTessellation=0
+        os.remove('Rads.txt')
+        os.remove('Centers.txt')
+    ################################################################
+    ######Extraction of middle Representative volume element########
+    ################################################################
+    myfile12=os.path.join(mypath,'RVE27.stver')
+    verX=np.loadtxt(myfile12)
+    NumOfNodes=len(verX)
+    myfile12=os.path.join(mypath,'RVE27.stedge')
+    EdgesLength=np.loadtxt(myfile12)
+    NumOfEdges=len(EdgesLength)
+    myfile12=os.path.join(mypath,'RVE27.stface')
+    SurfacesArea=np.loadtxt(myfile12)
+    NumOfSurfaces=len(SurfacesArea)
+    myfile12=os.path.join(mypath,'RVE27.stcell')
+    CellVolumes=np.loadtxt(myfile12)
+    NumOfVolumes=len(CellVolumes)
+    ####   Reading the GEO file
+    myfile12=os.path.join(mypath,'RVE27.geo')
+    text_file = open(myfile12, "r")
+    lines = text_file.readlines()
+    ####   Extraction of Nodes
+    t=0
+    Nodes=list(range(NumOfNodes))
+    for i in range(0,NumOfNodes):
+        currentline = lines[i].split("{")
+        a=currentline[1].split("}")
+        b=a[0].split(",")
+        c=np.array(b)
+        Nodes[t]=np.absolute(c.astype(np.float))
+        t=t+1
+    #####   Extraction of Edges
+    Edges=list(range(NumOfEdges))
+    r=0
+    t=0
+    for i in range(NumOfNodes,NumOfNodes+NumOfEdges):
+        currentline = lines[i].split("{")
+        a=currentline[1].split("}")
+        b=a[0].split(",")
+        c=np.array(b)
+        Edges[t]=np.absolute(c.astype(np.float))
+        t=t+1
+    ####   Extraction of Faces
+    Faces=list(range(NumOfSurfaces))
+    r=0
+    t=0
+    for i in range(0,NumOfSurfaces):
+        j=NumOfNodes+NumOfEdges+(2*i)
+        currentline = lines[j].split("{")
+        a=currentline[1].split("}")
+        b=a[0].split(",")
+        c=np.array(b)
+        Faces[t]=np.absolute(c.astype(np.float))
+        t=t+1
+    ####   Extraction of volumes
+    Volumes=list(range(NumOfVolumes))
+    a=list(lines[NumOfNodes+NumOfEdges+(2*NumOfSurfaces)])
+    a[0:21]=''
+    o=len(a)
+    a[o-3:]=''
+    lines[NumOfNodes+NumOfEdges+(2*NumOfSurfaces)]="".join(a)
+    currentline=lines[NumOfNodes+NumOfEdges+(2*NumOfSurfaces)].split(",")
+    c=np.array(currentline)
+    Volumes[0]=np.absolute(c.astype(np.float))
+    t=1
+    for i in range(1,NumOfVolumes):
+        j=NumOfNodes+NumOfEdges+(2*NumOfSurfaces)+i
+        currentline = lines[j].split(" = {")
+        a=currentline[2].split("}")
+        b=a[0].split(",")
+        c=np.array(b)
+        Volumes[t]=np.absolute(c.astype(np.float))
+        t=t+1
+    #####################################################
+    MAX0=list(range(0,NumOfCells))
+    for i in range(NumOfCells):
+        MAX0[i]=max(Volumes[i])
+    MaxIndexOfFaces=max(MAX0)
+    MAX1=list(range(0,int(MaxIndexOfFaces)))
+    for i in range(int(MaxIndexOfFaces)):
+        MAX1[i]=max(Faces[i])
+    MaxIndexOfEdges=max(MAX1)
+    MAX2=list(range(0,int(MaxIndexOfEdges)))
+    for i in range(int(MaxIndexOfEdges)):
+        MAX2[i]=max(Edges[i])
+    MaxIndexOfNodes=max(MAX2)
+    text_file.close()
+    ####################################################
+    # Making GEO file containing Periodic RVE
+    myfile13=os.path.join(mypath,'PeriodicRVE.geo')
+    text_GEO = open(myfile13, "w")
+    text_GEO.write('{0}{1}{2}\n'.format('cl=',MeshSize,';'))
+    for i in range(int(MaxIndexOfNodes)):
+        lines[i] = lines[i].replace('};',',cl};')
+        text_GEO.write('{0}'.format(lines[i]))
+    for i in range(int(NumOfNodes),int(NumOfNodes+MaxIndexOfEdges)):
+        text_GEO.write('{0}'.format(lines[i]))
+    j=int(NumOfNodes+NumOfEdges)
+    for i in range(int(NumOfNodes+NumOfEdges),int(NumOfNodes+NumOfEdges+MaxIndexOfFaces)):
+        text_GEO.write('{0}{1}'.format(lines[j],lines[j+1]))
+        j=2+j
+    text_GEO.write('{0}{1}{2}\n'.format(' Surface Loop (1) = {',lines[int(NumOfNodes+NumOfEdges+(2*NumOfSurfaces))],'};'))
+    NumOfCells=NumOfVolumes/27
+    j=int(NumOfNodes+NumOfEdges+(2*NumOfSurfaces)+1)
+    for i in range(int(NumOfCells-1)):
+        text_GEO.write('{0}'.format(lines[j]))
+        j+=1
+    text_GEO.write('{0}{1}{2}{3}{4}'.format('Volume (',NumOfCells,') = {',NumOfCells,'};'))
+    text_GEO.close()
+    ################################ End of Geometry construction##################################
+    os.remove('RVE27.geo')
+    os.remove('RVE27.stcell')
+    os.remove('RVE27.stedge')
+    os.remove('RVE27.stface')
+    os.remove('RVE27.stver')
+    ######################################################################
+                      # Creating Mesh Using GMSH
+    ######################################################################
+    commandMeshing="gmsh PeriodicRVE.geo -2 -optimize_lloyd -o PeriodicRVE -format inp -saveall"
+    os.system(commandMeshing)
+    time.sleep(15)
+    os.remove('PeriodicRVE.geo')
     files = os.listdir('.')
     if ('PeriodicRVE.inp' in files):
+        SuccessInMeshing=0
         break
 ######################################################################
      # Creating Corrected INP To Be processed In Next Step
@@ -678,7 +690,6 @@ while True:
 os.chdir(mypath)
 os.remove('INP.inp')
 os.remove('PBC.txt')
-os.remove('PeriodicRVE.geo')
 os.chdir(D)
 NumberOfCPUs=commands.getoutput("nproc")
 CMDforABAQUS='cd {0} && ./{1} job=INP cpus={2}'.format(D,A,NumberOfCPUs)
