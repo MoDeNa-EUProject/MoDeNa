a43b7b2566a2dd4e95d8787b0dddd58cc5da3d50
Mohsen <mohsen.karimi@polito.it>
2016-02-04 09:24:46 +0100

    CFD_tool_0D, instantiate and allocate memory for reaction_mixture_density

--- applications/PUfoam/MoDeNaModels/CFD_tool_0D/src/modenaCalls.h
+++ applications/PUfoam/MoDeNaModels/CFD_tool_0D/src/modenaCalls.h
@@ -1,5 +1,5 @@
 /** @file modenaCalls.h
-	@brief instantiates the surrogate models, allocates memory and fetches arg positions
+    @brief instantiates the surrogate models
 */
 #ifndef MODENACALLS_H
 #define MODENACALLS_H
@@ -13,52 +13,39 @@ instantiate the surrogate models:
 */
 
 bblgr1 = modena_model_new("bubbleGrowth1");
-if(modena_error_occurred())
+if (modena_error_occurred())
 {
     return modena_error();
 }
 
 bblgr2 = modena_model_new("bubbleGrowth2");
-if(modena_error_occurred())
+if (modena_error_occurred())
 {
     return modena_error();
 }
-
-// modena_model_t *rheologymodel = modena_model_new("viscosity_Arrhenius");
 
 kinetics = modena_model_new("RF-1-public");
-if(modena_error_occurred())
+if (modena_error_occurred())
 {
     return modena_error();
 }
 
-// modena_model_t *density_reaction_mixturemodel = modena_model_new("density_reaction_mixtureSM");
-// modena_model_t *rheologymodel = modena_model_new("rheology");
-// modena_model_t *kinetics = modena_model_new("simpleKinetics");
-/* allocate memory and fetch arg positions:
-    - bubbleGrowth1,
-    - bubbleGrowth2,
-    - density_reaction_mixture,
-    - rheology.
-*/
-inputs_bblgr1     = modena_inputs_new(bblgr1);
-outputs_bblgr1   = modena_outputs_new(bblgr1);
+density_reaction_mixturemodel = modena_model_new("density_reaction_mixture");
+if (modena_error_occurred())
+{
+    return modena_error();
+}
 
-inputs_bblgr2     = modena_inputs_new(bblgr2);
-outputs_bblgr2   = modena_outputs_new(bblgr2);
+inputs_bblgr1 = modena_inputs_new(bblgr1);
+outputs_bblgr1 = modena_outputs_new(bblgr1);
 
-// modena_inputs_t *inputs_rheo       = modena_inputs_new (rheologymodel);
-// modena_outputs_t *outputs_rheo     = modena_outputs_new (rheologymodel);
+inputs_bblgr2 = modena_inputs_new(bblgr2);
+outputs_bblgr2 = modena_outputs_new(bblgr2);
 
-inputs_kinetics   = modena_inputs_new(kinetics);
+inputs_kinetics = modena_inputs_new(kinetics);
 outputs_kinetics = modena_outputs_new(kinetics);
 
-// modena_inputs_t *inputs_den        = modena_inputs_new (density_reaction_mixturemodel);
-// modena_outputs_t *outputs_den      = modena_outputs_new (density_reaction_mixturemodel);
+inputs_den = modena_inputs_new (density_reaction_mixturemodel);
+outputs_den = modena_outputs_new (density_reaction_mixturemodel);
 
-// modena_inputs_t *inputs_rheo       = modena_inputs_new (rheologymodel);
-// modena_outputs_t *outputs_rheo     = modena_outputs_new (rheologymodel);
-
-// modena_inputs_t *inputs_kinetics   = modena_inputs_new (kinetics);
-// modena_outputs_t *outputs_kinetics = modena_outputs_new (kinetics);
 #endif
