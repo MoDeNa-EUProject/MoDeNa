344383aebc792659c96d781f6f912e1da6162e6d
mohsen <mohsenk@outlook.com>
2016-05-18 12:08:03 +0200

    adding examples for foamExpansion

---
+++ applications/PUfoam/foamExpansion/examples/BaserKinetics/inputs/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# export PKG_CONFIG_PATH=${PKG_CONFIG_PATH:-}:${HOME}/lib/pkgconfig:/usr/local/lib/pkgconfig
+# export PYTHONPATH=${PYTHONPATH:-}:${HOME}/lib/python2.7/site-packages/modena
+# export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-}:${HOME}/lib/python2.7/site-packages:${HOME}/lib/modena:/usr/local/lib
+cp -r ../inputs/* ./
+# cp -r ../inputs/constant ./
+# cp -r ../inputs/system ./
+# cp ../inputs/getMeReady.sh ./
+# cp ../inputs/case.foam ./
+./getMeReady.sh
+blockMesh
+setFields
+rm -fv log
+echo "MODENAFoam is running..."
+${FOAM_USER_APPBIN}/MODENAFoam >& log
+echo "Post-processing..."
+probeLocations
+cd postProcessing/probes/0/
+for file in $(ls *);
+        do
+                sed '/^#/d' $file > $file.txt;
+        done
+ls > list
+egrep -v '*txt' list > list2
+mv list2 list
+rm -rf $(<list)
+cp *.txt ../../../../results/CFD3D/
+echo "Done."
