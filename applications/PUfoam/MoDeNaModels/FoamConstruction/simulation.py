d61d132852297cc9d13c540b81a809c7aa40b78e
Pavel Ferkl <pavel.ferkl@gmail.com>
2017-05-02 17:07:10 +0200

    periodic boundary conditions in fenics simulation

--- applications/PUfoam/MoDeNaModels/FoamConstruction/simulation.py
+++ applications/PUfoam/MoDeNaModels/FoamConstruction/simulation.py
@@ -3,9 +3,17 @@
 @brief      Spatially thee-dimensional model of heat/mass transfer.
 @author     Pavel Ferkl
 """
+from __future__ import print_function
 from dolfin import *
+from dolfin import near, SubDomain, File, MeshFunction, interactive
 import subprocess as sp
 from blessings import Terminal
+XMIN = 0.0
+XMAX = 1.0
+YMIN = 0.0
+YMAX = 1.0
+ZMIN = 0.0
+ZMAX = 1.0
 def main():
     """Main function."""
     fname = "test"
@@ -36,15 +44,35 @@ def convert_mesh(input_mesh, output_mesh):
     call = sp.Popen(['dolfin-convert', input_mesh, output_mesh])
     call.wait()
 
-# class PeriodicBoundary(SubDomain):
-#     """Sub domain for Periodic boundary condition."""
-#     def inside(self, z, on_boundary):
-#         """Top boundary is "target domain" G"""
-#         return bool(z[2] < DOLFIN_EPS and z[2] > -DOLFIN_EPS and on_boundary)
-#     # Map bottom boundary (H) to top boundary (G)
-#     def map(self, x, y, z):
-#         y[0] = x[0] - 1.0
-#         y[1] = x[1]
+class PeriodicDomain(SubDomain):
+    """Class for periodic boundary conditions."""
+    def inside(self, x, on_boundary):
+        """
+        return True if on left (XMIN) or front (YMIN) boundary AND NOT on one of
+        the two slave edges
+        """
+        return bool((near(x[0], XMIN) or near(x[1], YMIN)) and
+            (not ((near(x[0], XMAX) and near(x[1], YMIN)) or
+                  (near(x[0], XMIN) and near(x[1], YMAX)))) and on_boundary)
+
+    def map(self, x, y):
+        """map y onto x"""
+        if near(x[0], XMAX) and near(x[1], YMAX):
+            y[0] = x[0] - (XMAX - XMIN)
+            y[1] = x[1] - (YMAX - YMIN)
+            y[2] = x[2]
+        elif near(x[0], XMAX):
+            y[0] = x[0] - (XMAX - XMIN)
+            y[1] = x[1]
+            y[2] = x[2]
+        elif near(x[1], YMAX):
+            y[0] = x[0]
+            y[1] = x[1] - (YMAX - YMIN)
+            y[2] = x[2]
+        else:
+            y[0] = -1000
+            y[1] = -1000
+            y[2] = -1000
 
 def preprocess(fname):
     mesh = Mesh(fname+".xml")
@@ -54,9 +82,11 @@ def preprocess(fname):
     plot(mesh)
     plot(boundaries)
     plot(subdomains)
-    interactive()
-    V = FunctionSpace(mesh, "CG", 2)
-    # V = FunctionSpace(mesh, "CG", 1, constrained_domain=PeriodicBoundary())
+    # interactive()
+    V = FunctionSpace(mesh, "CG", 1)
+    V = FunctionSpace(mesh, "CG", 1, constrained_domain=PeriodicDomain())
+    dofmap = V.dofmap()
+    print('Number of dofs', len(dofmap.dofs()))
     bctop = Constant(1)
     bcbot = Constant(2)
     bc = [
@@ -65,13 +95,26 @@ def preprocess(fname):
     ]
     u = Function(V)
     v = TestFunction(V)
-    D = 1
-    F = inner(D*grad(u), grad(v))*dx
+    D = Constant(1)
+    # f = Expression(
+    #     '1/(pow(x[0]-0.1,2)+pow(x[1]-0.1,2)+pow(x[2]-0.1,2)+1e-8)',
+    #     degree=2
+    # )
+    F = -D*inner(grad(u), grad(v))*dx# + f*v*dx
     return F, u, bc
 
 def integrate(F, u, bc):
     """Integrates the equation"""
+    # a, L = lhs(F), rhs(F)
+    # solve(a == L, u, bc)
     solve(F == 0, u, bc)
+    # check periodicity
+    # print(u(0.0, 0.1, 0.1))
+    # print(u(1.0, 0.1, 0.1))
+    # print(u(0.1, 0.0, 0.1))
+    # print(u(0.1, 1.0, 0.1))
+    # print(u(0.1, 0.1, 0.0))
+    # print(u(0.1, 0.1, 1.0))
     return u
 
 def postprocess(fname, u):
